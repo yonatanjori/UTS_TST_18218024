1f556bc0cd85c45c3bfcb12528b95c2cdbff580b
yonatanjori <53529645+yonatanjori@users.noreply.github.com>
2020-10-19 19:32:07 +0700

    Add files via upload
    komponen dalam, menggunakan scss css tsx, changing alignment and color

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,10 @@ const Header = () : ReactElement => {
   return (
     <div className={styles.container}>
       <div className={styles.title}>
-        Web Application Simple Form-DB Integration UTS II3160 Teknologi Sistem Terintegrasi
+        Simple Web Application With DB Integration
+      </div>
+      <div className={styles.title}>
+        UTS II3160 Teknologi Sistem Terintegrasi
       </div>
       <div className={styles.title}>
         18218024 Yonatan Jori Saputro
